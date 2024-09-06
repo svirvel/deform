d373322e4a8c53702438b2b6cda21440ac08e4e5
Alice Nilsson <alice.nilsson@vgregion.se>
2024-09-06 13:53:52 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import sys
 import platform
 import subprocess
@@ -8,30 +9,10 @@ from pprint import pprint
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
-with open("README.md", encoding="utf-8") as f:
-    readme = f.read()
-
-# Parse command line flags
-flags = {k: 'OFF' for k in ['--debug', '--use-cuda', '--use-ispc', '--use-itk']}
-for flag in flags.keys():
-    if flag in sys.argv:
-        flags[flag] = 'ON'
-        sys.argv.remove(flag)
-
-# Command line flags forwarded to CMake
-cmake_cmd_args = []
-for f in sys.argv:
-    if f.startswith('-D'):
-        cmake_cmd_args.append(f)
-
-for f in cmake_cmd_args:
-    sys.argv.remove(f)
-
-
 class CMakeExtension(Extension):
-    def __init__(self, name, cmake_lists_dir=''):
-        Extension.__init__(self, name, sources=[])
-        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
 
 
 class CMakeBuild(build_ext):
@@ -47,7 +28,9 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
 
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cfg = 'Debug' if flags['--debug'] == 'ON' else 'Release'
+
+        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "Release"
         build_args = ['--config', cfg]
 
         cmake_args = [
@@ -56,24 +39,26 @@ class CMakeBuild(build_ext):
             '-DDF_BUILD_TESTS=OFF',
             '-DDF_BUILD_WITH_DEBUG_INFO=%s' % ('ON' if cfg == 'Debug' else 'OFF'),
             '-DCMAKE_BUILD_TYPE=%s' % cfg,
-            '-DDF_USE_CUDA=%s' % flags['--use-cuda'],
-            '-DDF_USE_ISPC=%s' % flags['--use-ispc'],
-            '-DDF_ITK_BRIDGE=%s' % flags['--use-itk'],
             '-DPYTHON_EXECUTABLE=' + sys.executable,
             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
         ]
 
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+
         if platform.system() == "Windows":
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
 
-        cmake_args += cmake_cmd_args
         pprint(cmake_args)
 
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
+        build_temp = Path(self.build_temp) / ext.name
+        if not build_temp.exists():
+            build_temp.mkdir(parents=True)
 
-        subprocess.check_call(['cmake', ext.cmake_lists_dir] + cmake_args, cwd=self.build_temp)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.run(['cmake', ext.sourcedir, *cmake_args], cwd=build_temp, check=True)
+        subprocess.run(['cmake', '--build', '.', *build_args], cwd=build_temp, check=True)
 
 
 setup(
